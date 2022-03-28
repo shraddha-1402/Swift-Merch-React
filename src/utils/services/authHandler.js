b340dcc218a43f0b392656689396fe853d6fdf4c
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-03-28 11:58:23 +0530

    feat: setwishlist case added

--- src/utils/services/authHandler.js
+++ src/utils/services/authHandler.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 import { actionType } from "../../constants";
 
-const authHandler = async (credentials, authAction, userDispatch) => {
+const authHandler = async (
+  credentials,
+  authAction,
+  userDispatch,
+  productsDispatch
+) => {
   try {
     const response = await axios.post(`/api/auth/${authAction}`, {
       ...credentials,
@@ -14,11 +19,16 @@ const authHandler = async (credentials, authAction, userDispatch) => {
     localStorage.setItem("token", response.data.encodedToken);
     localStorage.setItem("userData", JSON.stringify(userData));
 
-    if (response.status === 201 || response.status === 200)
+    if (response.status === 201 || response.status === 200) {
       userDispatch({
         type: actionType.USER_ACTION.SAVE_USER_DATA,
         payload: { ...userData, token: response.data.encodedToken },
       });
+      productsDispatch({
+        type: actionType.PRODUCT_ACTIONS.SET_WISHLIST,
+        payload: userData.wishlist,
+      });
+    }
     return {
       status: response.status,
     };
