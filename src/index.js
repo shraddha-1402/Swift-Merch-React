d9347a02c2227f3936aa10618e02353c85199384
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-08 23:20:53 +0530

    removed userdataprovider context

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
-import {
-  DataProvider,
-  FilterProvider,
-  UserDataProvider,
-  AuthProvider,
-} from "./context";
+import { DataProvider, FilterProvider, AuthProvider } from "./context";
 
 // Call make Server
 makeServer();
@@ -18,13 +13,11 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <UserDataProvider>
-          <DataProvider>
-            <FilterProvider>
-              <App />
-            </FilterProvider>
-          </DataProvider>
-        </UserDataProvider>
+        <DataProvider>
+          <FilterProvider>
+            <App />
+          </FilterProvider>
+        </DataProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
