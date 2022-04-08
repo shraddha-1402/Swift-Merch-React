68c155b405a002880a03615a041b533293c14e31
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-08 13:46:58 +0530

    wrote proper async call with try catch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,20 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Header, CategoryCard } from "../components";
 
 const HomePage = () => {
   const [featuredCategories, setFeaturedCategories] = useState([]);
 
-  useEffect(async () => {
-    const response = await axios.get("/api/categories");
-    if (response.status === 200)
-      setFeaturedCategories(response.data.categories);
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data, status, statusText } = await axios.get("/api/categories");
+        if (status === 200) setFeaturedCategories(data.categories);
+        else throw new Error(`${statusText} ${status}`);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
   }, []);
 
   return (
