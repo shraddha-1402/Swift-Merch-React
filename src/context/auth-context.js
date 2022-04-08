af9d7d28d83a1f0497f4303e83dd0de716dcc02e
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-08 18:25:15 +0530

    fixed localstorage access

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,19 +1,17 @@
 import React, { createContext, useContext, useReducer } from "react";
-import { PropTypes } from "prop-types";
+import PropTypes from "prop-types";
 import { authReducer } from "../reducer";
 
-const localStorageData = JSON.parse(localStorage.getItem("ecomData"));
-
-const defaultAuthState = {
-  token: localStorageData?.token | null,
-  userInfo: localStorageData?.userInfo | {},
-};
-
-const AuthContext = createContext(defaultAuthState);
+const AuthContext = createContext({ token: null, userInfo: {} });
 const useAuth = () => useContext(AuthContext);
 
 const AuthProvider = ({ children }) => {
-  const [authState, authDispatch] = useReducer(authReducer, defaultAuthState);
+  const userData = JSON.parse(localStorage.getItem("ecommData"));
+  const [authState, authDispatch] = useReducer(authReducer, {
+    token: userData ? userData.token : null,
+    userInfo: userData ? userData.userInfo : {},
+  });
+
   return (
     <AuthContext.Provider value={{ authState, authDispatch }}>
       {children}
