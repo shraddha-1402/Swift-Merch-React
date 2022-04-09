25923ee41836ce9e080b7ab290f6896cf558797a
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-09 11:45:35 +0530

    removed user data context from export

--- src/context/index.js
+++ src/context/index.js
@@ -1,4 +1,3 @@
 export { useData, DataProvider } from "./data-context";
 export { FilterProvider, useFilter } from "./filter-context";
-export { useUserData, UserDataProvider } from "./user-data-context";
 export { useAuth, AuthProvider } from "./auth-context";
