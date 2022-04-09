253f663aea50eb968ad5002fe1223e2250aa7081
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-09 11:45:02 +0530

    removed unused cases

--- src/reducer/data-reducer.js
+++ src/reducer/data-reducer.js
@@ -3,85 +3,28 @@ import { actionType } from "../constants";
 const dataReducer = (state, action) => {
   const { type, payload } = action;
   const {
-    DATA: {
-      UPDATE_PRODUCTS,
-      TOGGLE_WISHLIST_PROPERTY,
-      SET_WISHLIST,
-      TOGGLE_CART_PROPERTY,
-      SET_CART,
-      INCREMENT_CART_QUANTITY,
-      DECREMENT_CART_QUANTITY,
-      RESET_PRODUCTS,
-    },
+    DATA: { UPDATE_PRODUCTS, SET_WISHLIST, SET_CART },
   } = actionType;
 
   switch (type) {
     case UPDATE_PRODUCTS: {
       return {
         ...state,
-        products: payload,
+        products: payload.products,
       };
     }
 
-    case TOGGLE_WISHLIST_PROPERTY: {
-      return {
-        products: state.products.map((product) =>
-          product._id === payload
-            ? { ...product, inWishlist: !product.inWishlist }
-            : product
-        ),
-      };
-    }
     case SET_WISHLIST:
       return {
         ...state,
         wishlist: payload.wishlist,
       };
 
-    case TOGGLE_CART_PROPERTY: {
-      return {
-        products: state.products.map((product) =>
-          product._id === payload
-            ? { ...product, inCart: !product.inCart, cartQuantity: 1 }
-            : product
-        ),
-      };
-    }
-
     case SET_CART:
       return {
         ...state,
         cart: payload.cart,
       };
-
-    case INCREMENT_CART_QUANTITY: {
-      return {
-        products: state.products.map((product) =>
-          product._id === payload
-            ? { ...product, cartQuantity: product.cartQuantity + 1 }
-            : product
-        ),
-      };
-    }
-    case DECREMENT_CART_QUANTITY: {
-      return {
-        products: state.products.map((product) =>
-          product._id === payload
-            ? { ...product, cartQuantity: product.cartQuantity - 1 }
-            : product
-        ),
-      };
-    }
-    case RESET_PRODUCTS: {
-      return {
-        products: state.products.map((product) => ({
-          ...product,
-          inWishlist: false,
-          inCart: false,
-          cartQuantity: 1,
-        })),
-      };
-    }
 
     default:
       return state;
