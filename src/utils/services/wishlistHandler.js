527b6a44b1eebfb675df62085f5f6a14ec665f57
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-09 11:23:30 +0530

    add and delete wishlist handlers added

--- src/utils/services/wishlistHandler.js
+++ src/utils/services/wishlistHandler.js
@@ -19,61 +19,39 @@ const getAllWishlistProducts = async ({ token, dataDispatch }) => {
   }
 };
 
-const wishlistHandler = async (
-  _id,
-  requestType,
-  token,
-  productsDispatch,
-  userDispatch
-) => {
-  switch (requestType) {
-    case "POST": {
-      try {
-        const response = await axios.post(
-          "/api/user/wishlist",
-          {
-            product: {
-              _id,
-            },
-          },
-          {
-            headers: { authorization: token },
-          }
-        );
-        if (response.status === 201) {
-          productsDispatch({ type: "TOGGLE_WISHLIST_PROPERTY", payload: _id });
-          userDispatch({
-            type: actionType.USER_ACTION.ADD_TO_WISHLIST,
-            payload: response.data.wishlist,
-          });
-        } else console.log("cannot add to wishlist");
-      } catch (error) {
-        console.log(error);
-      }
-      break;
-    }
-
-    case "DELETE": {
-      try {
-        const response = await axios.delete(`/api/user/wishlist/${_id}`, {
-          headers: { authorization: token },
-        });
-        if (response.status === 200) {
-          productsDispatch({ type: "TOGGLE_WISHLIST_PROPERTY", payload: _id });
-          userDispatch({
-            type: actionType.USER_ACTION.REMOVE_FROM_WISHLIST,
-            payload: _id,
-          });
-        }
-      } catch (error) {
-        console.log(error);
-      }
-      break;
-    }
-
-    default:
-      break;
+const addToWishlist = async ({ token, product, dataDispatch }) => {
+  try {
+    const { data, status, statusText } = await axios.post(
+      "/api/user/wishlist",
+      { product },
+      { headers: { authorization: token } }
+    );
+    if (status === 201)
+      dataDispatch({
+        type: actionType.DATA.SET_WISHLIST,
+        payload: { wishlist: data.wishlist },
+      });
+    else throw new Error(`${statusText} ${status}`);
+  } catch (error) {
+    console.log(error);
   }
 };
 
-export { wishlistHandler, getAllWishlistProducts };
+const deleteFromWishlist = async ({ token, product, dataDispatch }) => {
+  try {
+    const { data, status, statusText } = await axios.post(
+      `/api/user/wishlist/${product._id}`,
+      { headers: { authorization: token } }
+    );
+    if (status === 200)
+      dataDispatch({
+        type: actionType.DATA.SET_WISHLIST,
+        payload: { wishlist: data.wishlist },
+      });
+    else throw new Error(`${statusText} ${status}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getAllWishlistProducts, addToWishlist, deleteFromWishlist };
